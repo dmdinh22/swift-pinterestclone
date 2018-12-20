aca00fe42783be4788676443f72ab58ce6ea39bd
David Dinh <dmdinh22@gmail.com>
2018-12-20 00:00:30 -0800

    beam me up scotty

--- PinterestClone/ViewController.swift
+++ PinterestClone/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
