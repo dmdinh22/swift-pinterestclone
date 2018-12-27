5c65ad2d680ea3c4440f76cd05f8e0f8ea667560
David Dinh <dmdinh22@gmail.com>
2018-12-27 00:54:11 -0800

    create VM for photos from unsplash

---
+++ PinterestClone/ViewModels/ViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  ViewModel.swift
+//  PinterestClone
+//
+//  Created by David D on 12/27/18.
+//  Copyright © 2018 David D. All rights reserved.
+//
+
+import UIKit
+
+struct CellViewModel {
+    let image: UIImage
+}
+
+class ViewModel {
+    // MARK: - Properties
+    private let client: APIClient
+    private var photos: Photos = [] {
+        didSet {
+            self.fetchPhoto()
+        }
+    }
+    var cellViewModels: [CellViewModel] = []
+    
+    // MARK: - UI
+    
+    var isLoading: Bool = false {
+        didSet {
+            showLoading?()
+        }
+    }
+    
+    var showLoading: (() -> Void)?
+    var reloadData: (() -> Void)?
+    var showError: ((Error) -> Void)?
+    
+    init(client: APIClient) {
+        self.client = client
+    }
+    
+    func fetchPhotos() {
+        if let client = client as? UnsplashClient {
+            self.isLoading = true
+            
+            let endpoint = UnsplashEndpoint.photos(id: UnsplashClient.apiKey, order: .popular)
+            client.fetch(with: endpoint) { (response) in
+                switch response {
+                case .success(let photos):
+                    self.photos = photos
+                case .error(let error):
+                    self.showError?(error)
+                }
+            }
+        }
+    }
+    
+    private func fetchPhoto() {
+        self.photos.forEach { (photo) in
+            guard let imageData = try? Data(contentsOf: URL(string: "")!) else {
+                self.showError?(APIError.unknown)
+                return
+            }
+            
+            guard let image = UIImage(data: imageData) else {
+                self.showError?(APIError.unknown)
+                return
+            }
+            
+            self.cellViewModels.append(CellViewModel(image: image))
+        }
+    }
+}
